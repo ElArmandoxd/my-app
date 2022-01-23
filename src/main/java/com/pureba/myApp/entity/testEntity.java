71e17acf2ddec1d0a2beb4bd77311a17d25f564e
elarmandoxd <amd.ilustraciones@gmail.com>
2022-01-23 15:41:57 -0600

    my-app: descripcion

---
+++ src/main/java/com/pureba/myApp/entity/testEntity.java
@@ -0,0 +1,16 @@
+package com.pureba.myApp.entity;
+
+public class testEntity {
+    /*
+    * Propiedad de prueba para la muestra de compilacion del archivo testEntity.
+    * */
+    private String nombre;
+
+    public String getNombre(){
+     return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
